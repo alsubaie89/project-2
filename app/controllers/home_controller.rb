51f8c854fe067cea446e8627a3a83dfef976e1e0
Alsubaie89 <A_H_3_D_89@hotmail.com>
2019-12-25 15:07:26 +0300

    heroku project

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,11 @@
 class HomeController < ApplicationController
+    before_action :authenticate_user!, only: [:show]
     def index
-        
+        @cars = Car.all
     end
 
-    
+    def show
+        @cars = current_user.cars
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
 
   root to: "home#index"
   resources :cars
+  get '/my-cars', to:'home#show', as: "my_cars" # my_cars_path
 end
